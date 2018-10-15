81f7f930f59e7a7761d9507df98dc7e9e3fcac6a
tianle <tianle@meili-inc.com>
2018-10-15 19:38:07 +0800

    try

--- onecase.air/onecase.py
+++ onecase.air/onecase.py
@@ -5,6 +5,10 @@ from airtest.core.api import *
 
 auto_setup(__file__)
 
+print('swipe=========>')
+
 while 1==1:
     swipe((800, 1800), vector=[0, -0.5], duration=0.3)
-    print('swipe')
+    swipe((800, 1800), vector=[0, -0.5], duration=1)
+    swipe((800, 1800), vector=[0, -0.5], duration=0.3)
+    swipe((800, 1800), vector=[0, -0.5], duration=1)
